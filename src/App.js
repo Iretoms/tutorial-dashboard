ce3c47620f8885259bd677156edb9b9a74bd2d0e
iretoms <iretomiwajonathan@gmail.com>
2023-01-23 08:18:44 -0800

    pages and routes set up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import List from "./pages/list/List";
+import Login from "./pages/login/Login";
+import New from "./pages/new/New";
+import Single from "./pages/single/Single";
+
+
+
 
 function App() {
   return (
     <div className="App">
-     hello
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="users">
+            <Route index element={<List />} />
+            <Route path=":userId" element={<Single />} />
+            <Route path="new" element={<New />} />
+          </Route>
+        </Route>
+      </Routes>
     </div>
   );
 }
