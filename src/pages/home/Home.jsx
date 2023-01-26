7973b0c0dd8a5b16863bbf27809dcf8c1581acbf
iretoms <iretomiwajonathan@gmail.com>
2023-01-26 10:53:40 -0800

    navbar

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,15 @@
 import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
 import "./home.scss"
 
 const Home = () => {
     return (
         <div className="home">
            <Sidebar/> 
-           <div className="homeContainer">Home</div>
+           <div className="homeContainer">
+            <Navbar/>
+            home
+           </div>
         </div>
     );
 };
