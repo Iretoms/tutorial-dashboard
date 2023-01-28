98e9042f1a121c8b53f79b94f3c3dff18cf32266
iretoms <iretomiwajonathan@gmail.com>
2023-01-27 20:33:14 -0800

    Home table

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,7 +3,9 @@ import Navbar from "../../components/navbar/Navbar";
 import Widget from "../../components/widget/Widget";
 import Featured from "../../components/featured/Featured";
 import Chart from "../../components/chart/Chart";
+import List from "../../components/table/Table";
 import "./home.scss";
+
 
 const Home = () => {
   return (
@@ -20,6 +22,10 @@ const Home = () => {
         <div className="charts">
           <Featured />
           <Chart />
+        </div>
+        <div className="listContainer">
+          <div className="listTitle">Latest Transactions</div>
+          <List/>
         </div>
       </div>
     </div>
