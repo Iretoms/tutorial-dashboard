18553421eb73c27708b1bb41f1a9fd320c1ffa93
iretoms <iretomiwajonathan@gmail.com>
2023-01-30 22:17:47 -0800

    single page and some fix

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,11 +1,59 @@
-import "./single.scss"
+import "./single.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import Chart from "../../components/chart/Chart";
+import List from "../../components/table/Table"
 
 const Single = () => {
-    return (
-        <div>
-            Single
+  return (
+    <div className="single">
+      <Sidebar />
+      <div className="singleContainer">
+        <Navbar />
+        <div className="top">
+          <div className="left">
+            <div className="editButton">Edit</div>
+            <h1 className="title">Information</h1>
+            <div className="item">
+              <img
+                src="https://images.pexels.com/photos/103123/pexels-photo-103123.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt=""
+                className="itemImg"
+              />
+              <div className="details">
+                <h1 className="itemTitle">John Doe</h1>
+                <div className="detailItem">
+                  <span className="itemKey">Email:</span>
+                  <span className="itemValue">johndoe@gmail.com</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Phone:</span>
+                  <span className="itemValue">08028481021</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Address:</span>
+                  <span className="itemValue">
+                    1, Ajayi Crowther Street, Ikoyi, Lagos.
+                  </span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Country:</span>
+                  <span className="itemValue">Nigeria</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="right">
+            <Chart aspect={3 / 1} title="User Spending (Last 6 Months)" />
+          </div>
         </div>
-    );
+        <div className="bottom">
+             <h1 className="title">Last Transactions</h1>
+            <List/>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Single;
